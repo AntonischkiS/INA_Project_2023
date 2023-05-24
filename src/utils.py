f295688d46a87ce38d0c2c061ac9ccb86ce0e7c9
Niko Lemke <NikoTella>
2023-05-24 14:59:20 +0200

    implemented generating edges with only votes where both voters were present and generated Graphs

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ from collections import deque
 import networkx as nx
 
 BUNDESTAG_DATA_PATH = "data/Bundestag/"
-BUNDESTAG_GRAPH_PATH = "graphs/Bundestag/"
+BUNDESTAG_GRAPH_PATH = "graphs/Bundestag_only_Attendance/"
 YEAR_TO_LEG_ID = {"2021": 132, "2017": 111, "2013": 97, "2009": 83, "2005": 67}
 
 
