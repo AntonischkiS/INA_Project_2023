95e94e007053da6a64cbbfac578ccabdb9b31016
AntonischkiS <s.antonischki@campus.lmu.de>
2023-05-26 18:23:32 +0200

    Add folder to visualize

--- src/create_network.py
+++ src/create_network.py
@@ -5,6 +5,7 @@ import sys
 from tqdm import tqdm
 import utils
 
+
 # mandate = abgeordneter in 1 legislatur period
 
 def get_fraction_color(name):
@@ -50,6 +51,7 @@ def write_mandates_as_nodes(file_name, mandates):
         # info should be put before \n
     file.close()
 
+
 # test = {"factions": [{"id": 234, "name": 'DIE LINKE'}, {"id": 345, "name": 'SPD'}, ],
 #         "mandates": [{"id": 54325, "name": 'Peter Müller', "faction_id": 234},
 #                      {"id": 53264, "name": 'Angela Angel', "faction_id": 345}, ]}
@@ -68,12 +70,12 @@ def generate_edges(path, mandate_ids):
     output:
     list of tuples of the form (id, id, weight)
     """
-    VOTE_OPTIONS = ['yes', 'no', 'abstain', 'no_show'] 
+    VOTE_OPTIONS = ['yes', 'no', 'abstain', 'no_show']
     SV = {id: defaultdict(int) for id in mandate_ids}  # Same Votes count
     poll_file_names = list(filter(lambda p: p.startswith('poll'), os.listdir(path)))
-    #error_cnt=set()
+    # error_cnt=set()
     for pfname in tqdm(poll_file_names):
-        with open(path+'/'+pfname) as f:
+        with open(path + '/' + pfname) as f:
             pdata = json.load(f)
         # go through poll data and create a list for each vote option
         # that holds all mandate ids that voted this way
@@ -81,26 +83,25 @@ def generate_edges(path, mandate_ids):
         for vote in pdata['votes']:
             VC[vote['vote']].append(vote['mandate_id'])
 
-
         # do +1 in SV for each pair of mandate ids within each of the lists
         for voters in VC.values():
-            #print(voters)
+            # print(voters)
             lnv = len(voters)
             for i in range(lnv):
-                vo1 = voters[i] 
-                if vo1 not in mandate_ids: #this excludes everybody who left during a legislature period
-                    #error_cnt.add(vo1)
+                vo1 = voters[i]
+                if vo1 not in mandate_ids:  # this excludes everybody who left during a legislature period
+                    # error_cnt.add(vo1)
                     continue
-                for j in range(i+1, lnv):
+                for j in range(i + 1, lnv):
                     vo2 = voters[j]
                     if vo2 not in mandate_ids:
-                        #error_cnt.add(vo2)
+                        # error_cnt.add(vo2)
                         continue
-                    #assert vo1 in mandate_ids
+                    # assert vo1 in mandate_ids
                     SV[vo1][vo2] += 1
                     SV[vo2][vo1] += 1
-                  
-    #print(len(error_cnt))
+
+    # print(len(error_cnt))
     E = set()
     n_polls = len(poll_file_names)
     for midx, row in SV.items():
@@ -131,12 +132,11 @@ def generate_edges2(path, mandate_ids):
         # go through poll data and create a list for each vote option
         # that holds all mandate ids that voted this way
         VC = {vo: [] for vo in VOTE_OPTIONS}  # vote clusters
-        AV = [] #mandateids that attended vote
+        AV = []  # mandateids that attended vote
         for vote in pdata['votes']:
-            if (not vote['vote'] == 'no_show') and ( vote['mandate_id'] in mandate_ids) :
+            if (not vote['vote'] == 'no_show') and (vote['mandate_id'] in mandate_ids):
                 VC[vote['vote']].append(vote['mandate_id'])
                 AV.append(vote['mandate_id'])
-
 
         for vo1 in AV:
             for vo2 in AV:
@@ -181,11 +181,11 @@ def create_graph_file_from_legislature_data(leg_year, src_path, dst_path):
     os.makedirs(leg_dst_path, exist_ok=True)
 
     # nodes generieren
-    factions, mandates = parse_json_mandates(leg_src_path+"mandates.json")
+    factions, mandates = parse_json_mandates(leg_src_path + "mandates.json")
     # edges generieren
     print('generating edges')
-    #changed to generating edges 2
-    e_list = generate_edges2(leg_src_path,list(map(lambda x: x[2], mandates)))
+    # changed to generating edges 2
+    e_list = generate_edges2(leg_src_path, list(map(lambda x: x[2], mandates)))
     print('edges generated')
     # graph file erstellen
     file = open(leg_dst_path + f"network{leg_period}.net", "w")
@@ -201,10 +201,11 @@ def create_graph_file_from_legislature_data(leg_year, src_path, dst_path):
     # edges reinschreiben
     file.write("*edges " + str(len(e_list)) + "\n")
     for midx, midy, weight in e_list:
-        ix, iy  = MI_LOOKUP[midx], MI_LOOKUP[midy]
+        ix, iy = MI_LOOKUP[midx], MI_LOOKUP[midy]
         file.write(
-            str(ix +1) + " " + str(iy +1) + " " + str(weight)+ "\n") 
+            str(ix + 1) + " " + str(iy + 1) + " " + str(weight) + "\n")
     file.close()
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -215,4 +216,4 @@ if __name__ == '__main__':
         exit()
     leg_year = sys.argv[1]
     create_graph_file_from_legislature_data(leg_year, utils.BUNDESTAG_DATA_PATH, utils.BUNDESTAG_GRAPH_PATH)
-    #generate_edges("../abgeordnetenwatch/data/Bundestag/2021-2025", [1,2,3])
+    # generate_edges("../abgeordnetenwatch/data/Bundestag/2021-2025", [1,2,3])
